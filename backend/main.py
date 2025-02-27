df3e0830dada97dcd05c740f10124b92d46b4ce4
JonahPitcher <jonahlpitcher@gmail.com>
2025-02-27 16:52:31 -0600

    create backend

---
+++ backend/main.py
@@ -0,0 +1,35 @@
+# to start, run "source .venv/bin/activate"
+
+# run server with "fastapi dev main.py"
+# go to http://127.0.0.1:8000/items/5?q=somequery
+
+# docs at http://127.0.0.1:8000/docs
+# or at http://127.0.0.1:8000/redoc
+
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
